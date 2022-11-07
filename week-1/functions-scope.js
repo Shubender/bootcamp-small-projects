1fbe8e10aa0e38e9d5a19d3c8f0848500e38967e
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-11-07 18:13:28 +0100

    fix multiplyTen

--- week-1/functions-scope.js
+++ week-1/functions-scope.js
@@ -57,7 +57,8 @@ waitThenRun(myFunc);
 // Write a function that expects a number as an argument. If the value that is passed in is less than 0,
 // equal to 0, or not a number, the function should return the string 'ERROR'. If the number that is passed 
 // in is greater than or equal to 1000000 it should simply return the number. Otherwise it should multiply 
-// the number by 10 however many times it takes to get a number that is greater than or equal to 1000000 and return that.
+// the number by 10 however many times it takes to get a number that is greater than or equal to 1000000 and
+// return that.
 
 function multiplyTen(number) {
     if (number <= 0 || number !== number) {
@@ -72,7 +73,10 @@ function multiplyTen(number) {
     }
 }
 
- multiplyTen(10 / 'sd');
+ console.log(multiplyTen(10 / "sd"));
+
+ var results = multiplyTen(10);
+ console.log("The results is:", results);
 
 
 
