4d0d58ab235995898d5b42936f06501298e4bf6a
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-11-08 16:54:04 +0100

    tricker first day

---
+++ week-2/tricker/main.js
@@ -0,0 +1,56 @@
+//callback function will be executed right before the paint of the next frame
+// requestAnimationFrame(() => {
+//     console.log('log from request animation frame');
+// });
+
+// console.log('after request animation frame');
+
+// //you specify a number when callack function is executed
+// // setTimeout(() => {
+// //     console.log("log from setTimeout");
+// // }, 1000);
+
+// function doSomethingOnEachPaint() {
+//     requestAnimationFrame(() => {
+//         console.log('log before paint');
+//         doSomethingOnEachPaint();
+//     })
+// }
+
+// doSomethingOnEachPaint();
+
+
+const headlines = document.getElementById('headlines');
+let firstLink = document.querySelector("a");
+
+let widthOfHeadlines = headlines.offsetWidth;
+
+let currentLeftValue = widthOfHeadlines; /* initally set to the width of headlines. will be changed in moveLeft function */
+function moveLeft() {
+    requestAnimationFrame(() => {
+
+        if (headlines.offsetLeft + firstLink.offsetWidth < 0){
+            headlines.removeChild(firstLink);
+            headlines.appendChild(firstLink);
+            firstLink = document.querySelector("a");
+            currentLeftValue = 0;
+            headlines.style.left = currentLeftValue + "px";
+        }
+
+        headlines.style.left = currentLeftValue + "px";
+        currentLeftValue--;
+        moveLeft();
+
+    });
+}
+
+// Check every time your function is called if the sum of headlines.offsetLeft and firstLink.offsetWidthis 
+// smaller then 0
+
+// if it is smaller than 0:
+// remove the firstLink with removeChild from the headlines
+// append the firstLink with appendChild to the headlines
+// get and save the new firstLink with querySelector for example
+// reset the currentLeftValue to 0 and set the style.left of headlines to the new value
+
+moveLeft();
