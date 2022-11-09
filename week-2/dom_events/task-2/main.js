fdaa203ff4c44ac01aefce30816e06082ae716f8
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-11-09 16:47:04 +0100

    made task 2

---
+++ week-2/dom_events/task-2/main.js
@@ -0,0 +1,21 @@
+// Make a page that has a <textarea> element on it. As the user types visible characters into this field, 
+// the characters should be replaced with the characters in the corresponding position in the Gettysburg Address.
+// (Note - you can get and set the text in a <textarea> through its value property.)
+
+
+const textarea = document.querySelector("textarea");
+let count = 0;
+
+const newNextStr =
+    "Address Delivered at the Dedication of the Cemetery at Gettysburg Abraham Lincol November 19, 1863 Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this.";
+
+const arrText = newNextStr.split("");
+console.log("new array:", arrText);
+
+
+textarea.addEventListener("keydown", function (e) {
+    e.preventDefault();
+    textarea.value += arrText[count];
+    count++;
+});
+
