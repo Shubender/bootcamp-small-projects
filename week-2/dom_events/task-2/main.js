6cc6bd45e33bd541731f041009a1cd6491ddb2b9
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-11-09 18:15:47 +0100

    fix task 2

--- week-2/dom_events/task-2/main.js
+++ week-2/dom_events/task-2/main.js
@@ -9,13 +9,9 @@ let count = 0;
 const newNextStr =
     "Address Delivered at the Dedication of the Cemetery at Gettysburg Abraham Lincol November 19, 1863 Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this.";
 
-const arrText = newNextStr.split("");
-console.log("new array:", arrText);
-
-
-textarea.addEventListener("keydown", function (e) {
-    e.preventDefault();
-    textarea.value += arrText[count];
+textarea.addEventListener("keydown", function (event) {
+    event.preventDefault();
+    textarea.value += newNextStr[count];
     count++;
 });
 
