a5f7c6d38f31677e1603f7b3f5e15cef9d68fde4
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-11-24 16:41:09 +0100

    destruct task-3

---
+++ week-4/destructuring_rest_spread/task-3/main.js
@@ -0,0 +1,29 @@
+// Rewrite the following function to use destructuring assignment for the three variables it creates:
+
+//  function logInfo(city) {
+//      const name = city.name;
+//      const country = city.country;
+//      const numPeople = city.population;
+
+//      console.log(
+//          `${name} is in ${country} and has ${numPeople} inhabitants in it.`
+//      );
+//  }
+
+function logInfo(city) {
+    // const name = city.name;
+    // const country = city.country;
+    // const numPeople = city.population;
+
+    const [name, country, numPeople] = [
+        city.name,
+        city.country,
+        city.population,
+    ];
+
+    console.log(
+        `${name} is in ${country} and has ${numPeople} inhabitants in it.`
+    );
+}
+
+logInfo({ name: "Marseille", country: "France", population: 861635 });
