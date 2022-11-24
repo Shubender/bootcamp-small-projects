d347e9edcb2a78f61cadc0a482b1182195751e2e
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-11-24 16:26:58 +0100

    destr task-2

---
+++ week-4/destructuring_rest_spread/task-2/main.js
@@ -0,0 +1,21 @@
+// Write a function that takes two arrays as arguments and returns a new array containing all of the items
+//  in the two arrays passed to it. This function should
+
+// leave the original arrays unchanged
+
+// contain no loops of any kind. That includes for, for...in, for...of, while, or do...while
+
+// not call slice or any other method on the two arrays passed to it
+
+// not call push or concat on any array in any way
+
+let newArr;
+
+function addArr(arr1, arr2) {
+    return (newArr = [...arr1, ...arr2]);
+};
+
+const firstArr = [1, 3, 7, 4, 5];
+const secondArr = [2, 8, 1, 5, 7];
+addArr(firstArr, secondArr);
+console.log({ newArr });
