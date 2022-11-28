0b1cdd611e752f08186a660e1ce0621c1b87b5b1
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-11-28 17:08:31 +0100

    fs part 2

--- week-5/fun-with-fs/index.js
+++ week-5/fun-with-fs/index.js
@@ -10,27 +10,53 @@ const baseFilePath = path.join(myPath, "files");
 // const stat = fs.statSync(baseFilePath);
 // console.log("stat in Sync: ", stat);
 
-function logSizes(myPath) {
+// function logSizes(myPath) {
+//     const files = fs.readdirSync(myPath, { withFileTypes: true });
+//     console.log("My files: ", files);
+//     for (const element of files) {
+//         if (element.isFile()) {
+//             const filePath = myPath + "/" + element.name;
+//             fs.stat(filePath, (err, stats) => {
+//                 if (err) {
+//                     console.log("Error in stat");
+//                 }
+//                 console.log(filePath + ": " + stats.size + " bytes.");
+//             });
+//         }
+//         if (element.isDirectory()) {
+//             //console.log("Dir: ", element);
+//             let newDirPath = myPath + "/" + element.name;
+//             console.log("Dir: ", newDirPath);
+//             logSizes(newDirPath);
+//         }
+//     }
+// }
+
+// logSizes(baseFilePath);
+
+
+function mapSizes(myPath) {
     const files = fs.readdirSync(myPath, { withFileTypes: true });
-    console.log("My files: ", files);
+    //console.log("My files: ", files);
+    let newObj = {};
     for (const element of files) {
-        
         if (element.isFile()) {
-            const filePath = myPath + '/' + element.name;
-            fs.stat(filePath, (err, stats) => {
-                if (err) {
-                    console.log("Error in stat");
-                }
-                console.log(filePath + ': ' + stats.size + ' bytes.');
-            });  
+            const filePath = myPath + "/" + element.name;
+            const statsObj = fs.statSync(filePath);
+            newObj[element.name] = statsObj.size;
+            //console.log("new prop: ", newObj);
         }
         if (element.isDirectory()) {
-            //console.log("Dir: ", element);
-            let newDirPath = myPath + '/' + element.name;
-            console.log("Dir: ", newDirPath);
-            logSizes(newDirPath);
-        }    
+            let newDirPath = myPath + "/" + element.name;
+            //console.log("Dir: ", newDirPath);
+            newObj[element.name] = mapSizes(newDirPath);
+        }
     }
+    return newObj;
 }
 
-logSizes(baseFilePath);
+let strObj = JSON.stringify(mapSizes(baseFilePath), null, 4);
+console.log("saved");
+fs.writeFileSync("files.json", strObj);
+
+//console.log(mapSizes(baseFilePath));
