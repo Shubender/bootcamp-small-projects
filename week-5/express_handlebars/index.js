1e652fc0b791bfdc3bf2774332d0064431c49a39
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-12-05 15:38:51 +0100

    more_express_handlebars

--- week-5/express_handlebars/index.js
+++ week-5/express_handlebars/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const projects = require("./projects.json");
 
-console.log(projects);
+// console.log(projects);
 
 // Handlebars Setup
 const { engine } = require("express-handlebars");
@@ -20,7 +20,37 @@ app.get("/", (req, res) => {
     res.render("projects", {
         layout: "main",
         projects,
-        cohortName
+        cohortName,
+        showImage: true,
+    });
+});
+
+// :projectDirectory is a placeholder and will be put in req.params object
+
+app.get('/project/:projectDirectory', (req, res) => {
+    const projectDirectory = req.params.projectDirectory; // 'kitty-carousel';
+    const selectedProject = projects.find((p) => {
+        return p.path === projectDirectory;
+    });
+    // console.log("selectedProject:", selectedProject);
+
+    // TASK: check if selectedProject is undefined.
+    //      if it is undefined. set statuscode 404 and send response.
+    if (!selectedProject) {
+        res.statusCode = 404;
+        res.end();
+    }
+    
+    res.render("projects", {
+        projects,
+        showImage: false,
+        selectedProject: selectedProject,
+        cohortName,
+        helpers: {
+            toUpperCase(text) {
+                return text.toUpperCase();
+            },
+        },
     });
 });
 
